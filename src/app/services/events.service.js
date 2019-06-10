c647192e039cb79c6d28691c77970ab7ddfc47b2
petrtopor <coder.ru@gmail.com>
2019-06-10 05:07:55 +0400

    added external server address

--- src/app/services/events.service.js
+++ src/app/services/events.service.js
@@ -1,8 +1,12 @@
 import './events.module'
+const serverAddress = 'http://35.239.197.19/events-list'
+// const serverAddress = 'http://localhost:80/events-list'
 
 angular.module('eventsModule').service('eventsService', function($resource) {
   // this.allEvents = new Promise((resolve, reject) => resolve(['0', '1', '2']))
   // var events = $resource('http://localhost:80/events-list').get().$promise.then((smth) => console.log('Smth happened:\n', smth))
-  this.allEvents = $resource('http://localhost:80/events-list').get().$promise.then(smth => smth.eventsList)
-  this.getEvent = (eventId) => $resource('http://localhost:80/events-list').get().$promise.then(smth => smth.eventsList.find(event => event.id === eventId))
+  // this.allEvents = $resource('http://localhost:80/events-list').get().$promise.then(smth => smth.eventsList)
+  // this.getEvent = (eventId) => $resource('http://localhost:80/events-list').get().$promise.then(smth => smth.eventsList.find(event => event.id === eventId))
+  this.allEvents = $resource(serverAddress).get().$promise.then(smth => smth.eventsList)
+  this.getEvent = (eventId) => $resource(serverAddress).get().$promise.then(smth => smth.eventsList.find(event => event.id === eventId))
 })
